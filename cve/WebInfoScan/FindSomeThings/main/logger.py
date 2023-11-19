e1fa56bbd8b72e4ce42f4af61d81caa04599c48d
akyosk <135619239+akyosk@users.noreply.github.com>
2023-11-20 02:12:26 +0800

    Delete cve/WebInfoScan directory

--- cve/WebInfoScan/FindSomeThings/main/logger.py
+++
@@ -1,5 +0,0 @@
-
-
-def logging(info):
-    with open('./result/findSomeThingReport.txt', 'a+',encoding='utf-8') as f:
-        print(info,file = f)
--- cve/WebInfoScan/FindSomeThings/main/updata.py
+++
@@ -1,5 +0,0 @@
-
-def logging(info):
-    for i in range(10):
-        with open('./result/findSomeThingReport.txt', 'a+') as f:
-            print(info,file = f)
