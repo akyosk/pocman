38c53497c843576075beefd8210c9d2ae4b3cbd8
akyosk <135619239+akyosk@users.noreply.github.com>
2024-03-27 01:07:38 +0800

    Delete attack directory

--- attack/CheckCms.py
+++
@@ -1,6 +0,0 @@
-#!/user/bin/env python3
-# -*- coding: utf-8 -*-
-from attack.ImpPoc import Shiro_Poc
-def Check_Cms(cms,target,progress):
-    if cms == "shiro":
-        Shiro_Poc(target,progress)
